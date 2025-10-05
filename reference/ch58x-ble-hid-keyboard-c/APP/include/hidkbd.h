0899ce5e003ddc80fca50bdbfb2a58601716f3ee
Your Name <you@example.com>
2025-10-05 21:45:26 +0700

    x copy ch58x ble hid keyboard

---
+++ reference/ch58x-ble-hid-keyboard-c/APP/include/hidkbd.h
@@ -0,0 +1,66 @@
+/********************************** (C) COPYRIGHT
+ ******************************** File Name          : hidkbd.h Author : WCH
+ * Version            : V1.0
+ * Date               : 2018/12/10
+ * Description        :
+ *********************************************************************************
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ * Attention: This software (modified or not) and binary are used for
+ * microcontroller manufactured by Nanjing Qinheng Microelectronics.
+ *******************************************************************************/
+
+#ifndef HIDKBD_H
+#define HIDKBD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************************************************************
+ * INCLUDES
+ */
+
+#include <stdint.h>
+
+/*********************************************************************
+ * CONSTANTS
+ */
+
+// Task Events
+#define START_DEVICE_EVT 0x0001
+#define START_PARAM_UPDATE_EVT 0x0002
+#define START_PHY_UPDATE_EVT 0x0004
+#define START_KEYMAP_TICK_EVT 0x0008
+#define REPORT_KEYBOARD_EVT 0x0010
+#define REPORT_MOUSE_EVT 0x0020
+#define REPORT_CONSUMER_EVT 0x0040
+/*********************************************************************
+ * MACROS
+ */
+
+/*********************************************************************
+ * FUNCTIONS
+ */
+
+/*********************************************************************
+ * GLOBAL VARIABLES
+ */
+
+/*
+ * Task Initialization for the BLE Application
+ */
+extern void HidEmu_Init(void);
+
+/*
+ * Task Event Processor for the BLE Application
+ */
+extern uint16_t HidEmu_ProcessEvent(uint8_t task_id, uint16_t events);
+
+/*********************************************************************
+*********************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
